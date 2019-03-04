e301daee46db2c195b9a22e2322d3e765c25a0a6
Alonzocarlos95 <carlosalonzo95@hotmail.com>
2019-03-04 02:46:23 -0500

    storm center1

---
+++ assignments/lesson-8/java8/date8.js
@@ -0,0 +1,17 @@
+function loadDate() {
+    var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  
+  var n = weekday[d.getDay()];
+
+  var meses = new Array ("January","February","March","April","May","June","August","September","October","November","December");
+ var m= meses[d.getMonth()];
+    document.getElementById("fecha").innerHTML = n+', '+d.getDate()+' '+m+' '+d.getFullYear()+".";
+  }
