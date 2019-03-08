4bf4d5463139ad53cddcacff5d042f59377abe25
Alonzocarlos95 <carlosalonzo95@hotmail.com>
2019-03-08 01:54:58 -0500

    superhero

---
+++ assignments/lesson-8/java8/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybG9zbWFwOTUiLCJhIjoiY2pzemhoZGFmMWIzMTQ0b2Q1ZWE2Zng4aiJ9.9m1IoeJDR3XaXtIq8iPKeQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center:[-111.8766, 42.0963],
+zoom: 8
+});
