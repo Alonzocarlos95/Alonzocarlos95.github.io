2bbdb535b48116ef6135b462beb8d22da12f95c8
Alonzocarlos95 <carlosalonzo95@hotmail.com>
2019-03-21 20:01:04 -0500

    last changes

--- assignments/lesson10/scripts-10/jasonapi.js
+++ assignments/lesson10/scripts-10/jasonapi.js
@@ -20,3 +20,56 @@ weatherapi.onload = function() {
      document.getElementById("windChill").innerHTML = windChill;
 
 }
+//Forecast//
+var wforecast = new XMLHttpRequest;
+wforecast.open('GET','https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=f87436f313bd1ba72f7dccef0583c3e0&units=imperial', true);
+wforecast.send();
+wforecast.onload = function () {
+    var weatherdata = JSON.parse(wforecast.responseText);
+    console.log(weatherdata);
+
+    var listDate =[];
+    var listtemp= [];
+    var listIconCode = [];
+
+    for (i=0; i < weatherdata.list.length; ++i) {
+        time= weatherdata.list[i].dt_txt;
+        if (time.includes("18:00:00")) {
+
+            var date = new Date(weatherdata.list[i].dt * 1000);
+            var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+            var findDate = weekday[date.getDay()] + '<br>' + month[date.getMonth()] + ' ' + date.getDate();
+            listDate.push(findDate);
+       
+            var temp = weatherdata.list[i].main.temp;
+            var temp = Math.round(temp);
+            listtemp.push(temp);
+
+            var iconcode = weatherdata.list[i].weather["0"].icon;
+            var icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            listIconCode.push(icon_path);
+        }
+continue;
+    }
+    
+    document.getElementById('day1').innerHTML = listDate[0];
+    document.getElementById('day2').innerHTML = listDate[1];
+    document.getElementById('day3').innerHTML = listDate[2];
+    document.getElementById('day4').innerHTML = listDate[3];
+    document.getElementById('day5').innerHTML = listDate[4];
+    //Display corresponding weather icon
+    document.getElementById('weather_icon1').src = listIconCode[0];
+    document.getElementById('weather_icon2').src = listIconCode[1];
+    document.getElementById('weather_icon3').src = listIconCode[2];
+    document.getElementById('weather_icon4').src = listIconCode[3];
+    document.getElementById('weather_icon5').src = listIconCode[4];
+    //Display forecasted temperature
+    document.getElementById("highTemp1").innerHTML = listtemp[0];
+    document.getElementById("highTemp2").innerHTML = listtemp[1];
+    document.getElementById("highTemp3").innerHTML = listtemp[2];
+    document.getElementById("highTemp4").innerHTML = listtemp[3];
+    document.getElementById("highTemp5").innerHTML = listtemp[4];
+  
+   
+}
